bb3e4d777142753dd5ca8619eff620b7e12654b6
Camila Wolter <camila0wolter@gmail.com>
2023-03-24 18:41:46 -0300

    feat: implementação de um teste para a função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,7 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+from unittest.mock import patch, mock_open
 
 
 def test_counter():
-    pass
+    with patch("builtins.open", mock_open(read_data="Javascript Javascript")):
+        assert count_ocurrences("path", "JAVASCRIPT") == 2
