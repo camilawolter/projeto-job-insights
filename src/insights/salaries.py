69d2a4f07e898cf0db79f3bd8e823b1676ab3a46
Camila Wolter <camila0wolter@gmail.com>
2023-03-24 17:00:16 -0300

    feat: implementação do get_max_salary e get_min_salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,40 +1,33 @@
 from typing import Union, List, Dict
+from src.insights.jobs import read
 
 
 def get_max_salary(path: str) -> int:
-    """Get the maximum salary of all jobs
+    jobs_list = read(path)
+    max_salary = 0
 
-    Must call `read`
+    for job in jobs_list:
+        salary = job["max_salary"]
+        if salary and salary.isdigit():
+            salary = int(salary)
+            if salary > max_salary:
+                max_salary = salary
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    return max_salary
 
 
 def get_min_salary(path: str) -> int:
-    """Get the minimum salary of all jobs
+    jobs_list = read(path)
+    min_salary = float('inf')
 
-    Must call `read`
+    for job in jobs_list:
+        salary = job["min_salary"]
+        if salary and salary.isdigit():
+            salary = int(salary)
+            if salary < min_salary:
+                min_salary = salary
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    return min_salary
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
